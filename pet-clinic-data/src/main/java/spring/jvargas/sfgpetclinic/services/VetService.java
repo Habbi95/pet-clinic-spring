a0cfbfdbc4bf506305170584762c4ccaa635894b
Habbi95 <javiervargasvico@hotmail.com>
2019-06-12 15:21:27 +0200

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/spring/jvargas/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package spring.jvargas.sfgpetclinic.services;
+
+import spring.jvargas.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
