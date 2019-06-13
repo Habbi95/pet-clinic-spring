9ce6bcdf2b68b0d2a831750c98f19677d1a4a8e8
Habbi95 <javiervargasvico@hotmail.com>
2019-06-13 16:10:32 +0200

    Adding refactoring for Data Model Services. Closes #11.

--- pet-clinic-data/src/main/java/spring/jvargas/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/spring/jvargas/sfgpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package spring.jvargas.sfgpetclinic.services;
 
 import spring.jvargas.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
