daa050dddf2dbd70cb9fc9adbc0981c60acd5f89
Habbi95 <javiervargasvico@hotmail.com>
2019-06-20 13:31:13 +0200

    Add Vet List, updated controller and clean-coding. Closes #15

--- pet-clinic-web/src/main/java/spring/jvargas/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/spring/jvargas/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package spring.jvargas.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.jvargas.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vet/index.html";
     }
 }
