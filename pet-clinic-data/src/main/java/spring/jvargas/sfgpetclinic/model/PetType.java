b41e395b5594a17d942faa171545fcafdc926450
Habbi95 <javiervargasvico@hotmail.com>
2019-06-26 14:27:38 +0200

    Added PetTypeService and Map Impl. Refactor PetType to extend BaseEntity. Closes #29

--- pet-clinic-data/src/main/java/spring/jvargas/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/spring/jvargas/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package spring.jvargas.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
---
+++ pet-clinic-data/src/main/java/spring/jvargas/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package spring.jvargas.sfgpetclinic.services;
+
+import spring.jvargas.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
