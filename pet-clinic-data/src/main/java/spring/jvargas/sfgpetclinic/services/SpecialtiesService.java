dbe22113f98363a367afebc8055935deb81c8043
Habbi95 <javiervargasvico@hotmail.com>
2019-06-27 12:34:15 +0200

    Added Specialties Map Impl and Service. Closes #31

---
+++ pet-clinic-data/src/main/java/spring/jvargas/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package spring.jvargas.sfgpetclinic.services;
+
+import spring.jvargas.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
